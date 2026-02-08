7f395c4946fe62f90663703e013218ac0e9322a4
singlacodes <shivamsinglatechnical@gmail.com>
2026-02-08 20:40:36 +0530

    configured frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
+const App = () => {
+
+  return (
+    <>
+        <BrowserRouter>
+          <Routes>
+        <Route path="/"  element={<Home />}/>
+         <Route path="login"  element={<Login />}/>
+          <Route path="/register"  element={<Register />}/>
+          </Routes>
+        </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div className='bg-green-500 text-white p-8 text-4xl'>
+      Register - Testing Tailwind
+    </div>
+  )
+}
+
+export default Register
